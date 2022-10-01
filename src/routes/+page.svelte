467ba3bacac597f672c62ce28e3402bc8a3fdb22
Joachim Haagen Skeie <joachim@skeiene.no>
2022-10-01 12:45:38 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<header>
+    <img src="/images/its_makerspace_logo_white.png" />
+</header>
+
+<section class="main-section">
+    <h1>Hva er Makerspece?</h1>
+    <p class="justify">Makerspace kombinerer mekanikk, design og programmering i en kreativ aktivitet hvor fokuset ligger i å bruke egne evner til å produsere en kreativ gjenstand.</p>
+    <p class="justify">Et skaperverksted kan være så mangt og omfatter alt fra tradisjonelle systyer og sløydsaler til heldigitale moderne fabrikasjonsmetoder som 3d printing, laserkutting og digitale freser. </p>
+</section>
+
+<section class="image-section">
+    <img src="/images/skaperverksted_bilde_2.jpg" />
+</section>
+
+<section class="main-section">
+    <h1>ITS Makerspace</h1>
+    <p class="justify">ITS Makerspace holder til på UiO på Kjeller og har i skoleåret 2022/2023 tre tilbud: </p>
+    <ul>
+        <li>Junior Makerspace for barn fra 5-7 klassetrinn på onsdager fra kl. 16-18</li>
+        <li>Ungdomsmakerspace for barn fra 8-10 klassetrinn på fredager fra kl. 16-18</li>
+        <li>Videregåendemakerspace for barn fra 1-3 VGS klassetrinn på mandager fra kl. 17-19</li>
+    </ul>
+</section>
