29147ea846e4bcd02f836917ab4c2fbcbcaffa51
Joachim Haagen Skeie <joachim@skeiene.no>
2022-10-04 18:44:44 +0200

    La til tabell CSS. Oppdatert Jr siden

---
+++ _app/immutable/chunks/1-d7ecdd0c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-a0945edc.js";export{t as component};
---
+++ _app/immutable/chunks/4-eed8a8a3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/side/_slug_/_page.svelte-7fc935d3.js";export{t as component};
